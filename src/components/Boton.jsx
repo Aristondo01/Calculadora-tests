38fdf7f8768bd1c1170b0635a8123d89863ad147
Aristondo01 <sebastianaristondo@gmail.com>
2022-05-11 13:00:50 -0600

    Funcionalidad con borrado despues del segundo número

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,18 +1,10 @@
 import './Boton.css'
 import PropTypes from 'prop-types'
 
-const teclas = ['7', '8', '9', 'x', '4', '5', '6', '+', '1', '2', '3', '-', '0', '%', '.', '=']
+const teclas = ['C', 'CE', '+/-', '/', '7', '8', '9', 'x', '4', '5', '6', '+', '1', '2', '3', '-', '0', '%', '.', '=']
 
 const Boton = ({ agregarNum }) => (
     <div>
-        <div className="borrar">
-            <button key="Ce" type="button" className="Boton-gris-grande" onClick={() => { agregarNum('CE') }}>
-                <div className="texto">CE</div>
-            </button>
-            <button key="C" type="button" className="Boton-gris-grande" onClick={() => { agregarNum('C') }}>
-                <div className="texto">C</div>
-            </button>
-        </div>
         <div className="contenedor">
             {
                 teclas.map((tecla) => (
--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,38 +1,82 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import './Calculadora.css'
 import Boton from './Boton.jsx'
 import Pantalla from './Pantalla.jsx'
 
 const Calculadora = () => {
     const [pantallaview, setPantalla] = useState('')
-    const [operacion, setOperacion] = useState(['', '', ''])
-    const tam = useRef([0, 0])
+    const [operacion, setOperacion] = useState(['', '', '', false])
 
     const Calcular = () => {
-        if (operacion[2] !== '') {
-            console.log(operacion)
+        if (operacion[3] && operacion[0] !== '' && operacion[2] !== '') {
             if (operacion[1] === '-') {
                 const respuesta = parseFloat(operacion[0], 10) - parseFloat(operacion[2], 10)
-                setPantalla(respuesta, '')
-                setOperacion(['', '', ''])
+                if (respuesta > 999999999) {
+                    setPantalla('Error')
+                } else {
+                    setPantalla(respuesta.toString())
+                }
+                if (operacion[4] !== '=') {
+                    setOperacion([respuesta.toString(), operacion[4], '', false])
+                } else {
+                    setOperacion([respuesta.toString(), '', '', false])
+                }
             }
 
             if (operacion[1] === '+') {
                 const respuesta = parseFloat(operacion[0], 10) + parseFloat(operacion[2], 10)
-                setPantalla(respuesta, '')
-                setOperacion(['', '', ''])
+                if (respuesta > 999999999) {
+                    setPantalla('Error')
+                } else {
+                    setPantalla(respuesta.toString())
+                }
+                if (operacion[4] !== '=') {
+                    setOperacion([respuesta.toString(), operacion[4], '', false])
+                } else {
+                    setOperacion([respuesta.toString(), '', '', false])
+                }
             }
 
             if (operacion[1] === 'x') {
                 const respuesta = parseFloat(operacion[0], 10) * parseFloat(operacion[2], 10)
-                setPantalla(respuesta, '')
-                setOperacion(['', '', ''])
+                if (respuesta > 999999999) {
+                    setPantalla('Error')
+                } else {
+                    setPantalla(respuesta.toString())
+                }
+                if (operacion[4] !== '=') {
+                    setOperacion([respuesta.toString(), operacion[4], '', false])
+                } else {
+                    setOperacion([respuesta.toString(), '', '', false])
+                }
             }
 
             if (operacion[1] === '%') {
                 const respuesta = parseFloat(operacion[0], 10) % parseFloat(operacion[2], 10)
-                setPantalla(respuesta, '')
-                setOperacion(['', '', ''])
+                if (respuesta > 999999999) {
+                    setPantalla('Error')
+                } else {
+                    setPantalla(respuesta.toString(), '')
+                }
+                if (operacion[4] !== '=') {
+                    setOperacion([respuesta.toString(), operacion[4], '', false])
+                } else {
+                    setOperacion([respuesta.toString(), '', '', false])
+                }
+            }
+
+            if (operacion[1] === '/') {
+                const respuesta = parseFloat(operacion[0], 10) / parseFloat(operacion[2], 10)
+                if (respuesta > 0.00000009 && respuesta > 999999999) {
+                    setPantalla('Error')
+                } else {
+                    setPantalla(respuesta.toString().substring(0, 9))
+                }
+                if (operacion[4] !== '=') {
+                    setOperacion([respuesta.toString().substring(0, 9), operacion[4], '', false])
+                } else {
+                    setOperacion([respuesta.toString().substring(0, 9), '', '', false])
+                }
             }
         }
     }
@@ -41,30 +85,48 @@ const Calculadora = () => {
         Calcular()
     }, [pantallaview])
 
-    const agregarNum = ((num) => {
-        if (tam.current[0] < 9 && '0123456789'.includes(num)) {
+    const agregarNum = useCallback((num) => {
+        console.log(operacion)
+        if ('0123456789'.includes(num)) {
+            if (operacion[1] === '' && `${operacion[0]}`.length < 9) {
+                setOperacion([operacion[0] + num, '', '', false])
+                setPantalla(operacion[0] + num)
+            } else if (operacion[1] !== '' && `${operacion[2]}`.length < 9) {
+                setOperacion([operacion[0], operacion[1], operacion[2] + num, false])
+                setPantalla(operacion[2] + num)
+            }
+        } else if (num === '.' && !pantallaview.includes('.')) {
             setPantalla(pantallaview + num)
-            tam.current[0] += 1
-        } else if (num === '.' && tam.current[1] === 0) {
-            setPantalla(pantallaview + num)
-            tam.current[1] += 1
-        } else if ('x+-%'.includes(num) && operacion[0] === '') {
-            setOperacion([pantallaview, num, ''])
+            if (operacion[1] === '') {
+                setOperacion([operacion[0] + num, '', '', false])
+            } else {
+                setOperacion([operacion[0], operacion[1], operacion[2] + num, false])
+            }
+        } else if ('x+-%/'.includes(num) && operacion[1] === '') {
+            setOperacion([operacion[0], num, operacion[2], false, '#'])
+        } else if ('x+-%=/'.includes(num) && operacion[1] !== '') {
+            setOperacion([operacion[0], operacion[1], operacion[2], true, num])
             setPantalla('')
-            tam.current[0] = 0
-            tam.current[1] = 0
-        } else if (num === '=' && operacion[0] !== '') {
-            setOperacion([operacion[0], operacion[1], pantallaview])
-            setPantalla('')
-            tam.current[0] = 0
-            tam.current[1] = 0
         } else if (num === 'C') {
             setPantalla('')
+            if (operacion[1] === '') {
+                setOperacion(['', '', '', false])
+            } else {
+                setOperacion([operacion[0], operacion[1], '', false])
+            }
         } else if (num === 'CE') {
-            setOperacion(['', '', ''])
+            setOperacion(['', '', '', false])
             setPantalla('')
+        } else if (num === '+/-' && `${pantallaview}`.length < 8) {
+            if (operacion[2] === '') {
+                setOperacion([-1 * parseFloat(operacion[0], 10), operacion[1], '', false, num])
+                setPantalla((-1 * parseFloat(operacion[0], 10)).toString())
+            } else {
+                setOperacion([operacion[0], operacion[1], `-${operacion[1]}`, false, num])
+                setPantalla((-1 * parseFloat(operacion[2], 10)).toString())
+            }
         }
-    })
+    }, [operacion])
 
     return (
         <div className="cuerpo">
